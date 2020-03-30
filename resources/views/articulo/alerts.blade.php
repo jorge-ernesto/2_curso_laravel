ec825eb998c80ac839c10d30ab1d13ad749e561f
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-03-30 17:04:17 -0500

    aaa

--- resources/views/articulo/alerts.blade.php
+++ resources/views/articulo/alerts.blade.php
@@ -8,7 +8,7 @@
 @endif
 
 {{-- Errores --}}
-@if(count($errors)>0)
+@if( $errors->any() )
     <div class="alert alert-danger">
         <ul class="mb-0">
             @foreach($errors->all() as $error)
