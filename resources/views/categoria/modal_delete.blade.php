d5677fe31a63c1a1eef18ba1fcfec732d9871256
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-03-29 22:00:07 -0500

    aaa

--- resources/views/categoria/modal_delete.blade.php
+++ resources/views/categoria/modal_delete.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal fade" id="modal_delete_{{ $value->idcategoria }}" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade animated fadeIn" id="modal_delete_{{ $value->idcategoria }}" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">                    
         <div class="modal-content">
             <div class="modal-header">                
--- resources/views/layout/sb_admin/head.blade.php
+++ resources/views/layout/sb_admin/head.blade.php
@@ -7,3 +7,6 @@
 <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
 <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+
+{{-- Personalizado uwu --}}
+<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
