63533acf91e15ce5a94b938cc4cffb632d8a710a
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-04-01 22:46:21 -0500

    aaa

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -25,7 +25,7 @@ class UsuarioController extends Controller
     }
 
     public function create(){
-        //
+        return view('usuario.create');
     }
 
     public function store(Request $request){
