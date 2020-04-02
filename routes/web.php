4f5d0251859cef8ede2ab65fcd5048a0ec5c5766
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-04-02 18:51:35 -0500

    aaa

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 /* Pruebas */
 Route::get('/test', function(){
-    return $roles = App\User::find(2)
-                   ->roles()->orderBy('id')
-                   ->get();
+    return $roles = App\User::find(1)
+                    ->roles()                    
+                    ->get();
 });
