9cf70fea76dc5d61fecece376ef1e38104e78016
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-04-03 01:52:10 -0500

    aaa

--- resources/views/layouts/sb_admin/role.blade.php
+++ resources/views/layouts/sb_admin/role.blade.php
@@ -2,26 +2,27 @@
 @foreach($dataUser as $key=>$value)                        
     @if($value->name == "ROLE_ADMIN")                            
         <?php
-        session(['categoria' => true]); 
-        session(['articulo' => true]); 
-        session(['cliente' => true]); 
-        session(['proveedor' => true]);  
+        session(['categoria' => true]);
+        session(['articulo' => true]);
+        session(['cliente' => true]);
+        session(['proveedor' => true]);
         session(['usuario' => true]);
         ?>
     @endif    
 
     @if($value->name == "ROLE_USER")                            
         <?php
-        // session(['categoria' => true]); 
-        // session(['articulo' => true]); 
-        session(['cliente' => true]); 
-        session(['proveedor' => true]);                                  
+        session(['categoria' => true]);
+        session(['articulo' => true]);
+        session(['cliente' => true]);
+        session(['proveedor' => true]);
         ?>
     @endif    
 
     @if($value->name == "ROLE_INVITADO")                            
         <?php
-        // session(['categoria' => true]);                             
+        session(['categoria' => true]);
+        session(['articulo' => true]);
         ?>
     @endif
 @endforeach
