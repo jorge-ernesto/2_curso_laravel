3597ca528602d28875add142294628986aad9b07
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-04-04 18:15:00 -0500

    aaa

--- resources/views/articulo/create.blade.php
+++ resources/views/articulo/create.blade.php
@@ -52,7 +52,7 @@
                             <div class="custom-file">
                                 <input type="file" name="imagen" class="custom-file-input" id="customFile">
                                 <label class="custom-file-label" for="customFile">Choose file</label>
-                              </div>
+                            </div>
                         </div>
                     </div>
                     <h4>
--- resources/views/articulo/edit.blade.php
+++ resources/views/articulo/edit.blade.php
@@ -57,7 +57,7 @@
                             <div class="custom-file">
                                 <input type="file" name="imagen" class="custom-file-input" id="customFile">
                                 <label class="custom-file-label" for="customFile">Choose file</label>
-                              </div>
+                            </div>
                         </div>
                         @if($dataArticulo['imagen'] != "")
                             <img src="{{ asset('imagenes/articulos/'.$dataArticulo['imagen']) }}" alt="" width="100" style="position: relative; top: -62px!important;">
