432b52f6b9fad30862b6f4948a8e59767590fdfa
Jorge Ernesto <jorge.cywdt@gmail.com>
2020-07-24 20:56:21 -0500

    aaa

--- resources/views/layouts/sb_admin/head.blade.php
+++ resources/views/layouts/sb_admin/head.blade.php
@@ -4,11 +4,11 @@
 <meta name="description" content="" />
 <meta name="author" content="" />
 <title>Dashboard - SB Admin</title>
-<link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
-<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/DataTables/dataTables.bootstrap4.min.css') }}">
+<script src="{{ asset('assets/plugins/FontAwesome/all.min.js') }}"></script>
 
 {{-- Personalizado uwu --}}
-<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css"> <!-- Bootstrap Select -->
-<script src="https://cdn.jsdelivr.net/npm/js-cookie@2.2.0/src/js.cookie.js"></script> <!-- JavaScript Cookie -->
+<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/BootstrapSelect/bootstrap-select.min.css') }}"> <!-- Bootstrap Select -->
+<script src="{{ asset('assets/plugins/JavaScriptCookie/js.cookie.js') }}"></script> <!-- JavaScript Cookie -->
--- resources/views/layouts/sb_admin/scripts.blade.php
+++ resources/views/layouts/sb_admin/scripts.blade.php
@@ -1,13 +1,15 @@
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('assets/plugins/Jquery/jquery-3.4.1.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/Bootstrap/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('assets/js/scripts.js') }}"></script>
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+{{-- 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
 <script src="{{ asset('assets/demo/chart-area-demo.js') }}"></script>
 <script src="{{ asset('assets/demo/chart-bar-demo.js') }}"></script>
-<script src="{{ asset('assets/demo/chart-pie-demo.js') }}"></script> --}}
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('assets/demo/chart-pie-demo.js') }}"></script> 
+--}}
+<script src="{{ asset('assets/plugins/DataTables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/DataTables/dataTables.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('assets/demo/datatables-demo.js') }}"></script>
 
 {{-- Personalizado uwu --}}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script> <!-- Bootstrap Select -->
+<script src="{{ asset('assets/plugins/BootstrapSelect/bootstrap-select.min.js') }}"></script> <!-- Bootstrap Select -->
